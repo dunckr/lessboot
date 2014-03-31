725a02d1ef2fbf1671158d8e83b384adb5c3e514
Duncan Beaton <d@duncky.com>
2014-03-31 19:04:30 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lessboot::Application.config.secret_key_base = 'e84ea579df7929a1112dd921427628a91de00101e9d6292932ac01c64f7878903b52ae0a08112c22c5f0c36248b09235858ef968465c2579a3b894e44cee1d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lessboot::Application.config.session_store :cookie_store, key: '_lessboot_session'
